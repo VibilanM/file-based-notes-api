0c3a1f07cd96114bb87ea9340b46fa34007a2a8f
Vibilan <vibilan2793@gmail.com>
2026-05-26 20:31:34 +0530

    3/9

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({ message: "Hello World." });
 });
@@ -28,6 +30,28 @@ app.get('/notes', (req, res) => {
     });
 
 });
+
+app.post('/notes', (req, res) => {
+    const newNote = req.body;
+
+    fs.readFile('notes.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log("Error reading JSON file: ", err);
+        }
+
+        const notes = JSON.parse(data);
+
+        notes.push(newNote);
+
+        fs.writeFile('notes.json', JSON.stringify(notes, null, 2), (err) => {
+            if (err) {
+                console.log("Error writing into JSON file: ", err);
+            }
+
+            res.send("Note added successfully.");
+        })
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
